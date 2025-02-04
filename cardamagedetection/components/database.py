4455e9c96d2f02456026dda88080ed91e0c2b775
Rohit Mishra <rohitaniket09@gmail.com>
2025-02-04 18:59:09 +0530

    Update database.py

--- cardamagedetection/components/database.py
+++ cardamagedetection/components/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:1234@localhost:5432/cardamagedetection"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:134@localhost:5432/cadamagedetection"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
